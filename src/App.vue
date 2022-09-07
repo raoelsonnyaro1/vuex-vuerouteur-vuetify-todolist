7c34940a467ab8848f2c950bb6028ae4e8d11d27
raoelsonnyaro1 <raoelsonnyaro@gmail.com>
2022-09-07 05:43:05 +0300

    route

--- src/App.vue
+++ src/App.vue
@@ -27,16 +27,8 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <router-link to="/">Home</router-link>
-      <router-link to="/completed">Completed</router-link>
+      <v-btn color="primary" to="/">Home</v-btn>
+      <v-btn color="primary" to="/about">completed</v-btn>
     </v-app-bar>
 
     <v-main>
